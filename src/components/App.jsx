2155250d73d5c5a3eafb2ebcac831180814e2eb9
NazarKr <nazarok1990@gmail.com>
2023-02-10 19:29:50 +0200

    Update route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,16 +17,15 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SharedLayout />}>
+        <Route path="/" element={<SharedLayout />} >
           <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />}>
-            <Route path="movies/:movieId" element={<MovieDetails />}/>
-            <Route path="/movies/:movieId/cast" element={<Cast />} />
-            <Route path="/movies/:movieId/reviews" element={<Reviews />} />
-            <Route />
-            <Route path="*" element={<NotFound />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />} >
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="/about" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
       <ToastContainer />
