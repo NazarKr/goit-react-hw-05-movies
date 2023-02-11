c1db2e38af2d0475210415e862f47a64f0941e19
NazarKr <nazarok1990@gmail.com>
2023-02-11 17:34:45 +0200

    update route for git hub

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,14 +16,14 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SharedLayout />} >
+        <Route path="goit-react-hw-05-movies/" element={<SharedLayout />} >
           <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} >
+          <Route path="goit-react-hw-05-movies/movies" element={<Movies />} />
+          <Route path="goit-react-hw-05-movies/movies/:movieId" element={<MovieDetails />} >
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="/about" element={<About />} />
+          <Route path="goit-react-hw-05-movies/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
